19c8ec8f548c6074527507e7f52081ec2ff5b441
Vaishnavi <devagari.vaishnavi21@gmail.com>
2026-01-13 21:50:39 +0530

    first

---
+++ Functions/first.py
@@ -0,0 +1,22 @@
+def hello(name):
+    print("hello",name)
+
+hello("Vaishnavi")
+hello(123)
+
+def great_people(name,title):
+    print("hello",title,name)
+
+great_people("Vaishnavi",'MS')
+
+def sum(a,b):
+    return print(a+b)
+
+sum(10,5)
+
+
+a="10"
+b="20"
+print(a+b)
+
+
